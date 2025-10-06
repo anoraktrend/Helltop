be7d6cd7d04bac5ac849c2ee0de1b6a470e5a0c0
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2025-10-05 23:41:31 -0700

    continue moving forward
    Signed-off-by: Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>

--- .nuxt/types/build.d.ts
+++ .nuxt/types/build.d.ts
@@ -14,6 +14,8 @@ declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
 declare module "#build/color-mode-options.mjs";
 declare module "#build/image-options.mjs";
+declare module "#build/devtools/settings.mjs";
+declare module "#build/runtime.vue-devtools-client.sxveOa2dhX_t9FEbLWnlCi6f_qJ8sBpwb1m8g_mvIDM.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
