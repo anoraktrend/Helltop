2c52c75db7fbe89e50d6ffdcb70bc6ed74b10151
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2025-10-14 17:29:45 -0700

    Add mastohost to variables

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,7 +4,8 @@ export default defineAppConfig({
   socials: {
     liberapay: 'anoraktrend',
     github: 'anoraktrend/helltop',
-    mastodon: '@lucyinchat@tech.lgbt',
+    mastohost: 'tech.lgbt',
+    mastodon: '@lucyinchat',
     bluesky: 'lucy.helltop.net',
     codeberg: 'anoraktrend',
   },
