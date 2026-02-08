4a9ff76caa9876b713ae291eb858636fecddc8b4
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-02-08 10:30:02 -0800

    maybe fix the weird 404 screen.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,9 @@ export default defineNuxtConfig({
   colorMode: { classSuffix: '' },
 
   content: {
+    database: {
+      type: 'd1',
+    },
     build: {
       markdown: {
         highlight: {
