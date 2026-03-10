f9c8ef5e3bd24f1ee44f38cf47681e32e200cfb9
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-03-10 02:30:27 -0700

    UI: Switch default font to Space Mono for a cohesive terminal aesthetic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
       ]
     },
   },
