37ed97ad5b0eab3fe1f217a831a7f73642d647bf
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-03-09 20:26:29 -0700

    fix: use stem query instead of path for data collection
    Signed-off-by: Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>

--- app/pages/status.vue
+++ app/pages/status.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const { data: statusData } = await useAsyncData('subdomains', () => {
-  return queryCollection('data').path('/subdomains').first()
+  return queryCollection('data').where('stem', '=', 'subdomains').first()
 })
 
 interface Subdomain {
