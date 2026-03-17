678b6ae0cf0c2f6e64d5970cc2f99cb471b786fd
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-03-17 00:42:10 -0700

    even more changes
    Signed-off-by: Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,32 +9,32 @@ import alpinejs from '@astrojs/alpinejs';
 import db from '@astrojs/db';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: 'https://helltop.net',
   output: 'server',
-  adapter: netlify(), // This is the ONLY adapter you need for Netlify
-                            integrations: [svelte(), icon(), mdx(), alpinejs(), db()],
-                            vite: {
-                              resolve: {
-                                alias: {
-                                  'node-fetch': 'fetch',
-                                  debug: path.resolve(__dirname, './src/utils/debug-mock.js'),
-                                },
-                              },
-                              plugins: [
-                                tailwindcss(),
-                            Fonts({
-                              google: {
-                                families: [{
-                                  name: 'Space Mono',
-                                  styles: 'ital,wght@0,400;0,700;1,400;1,700',
-                                }],
-                              },
-                            }),
-                              ]
-                            }
+  adapter: cloudflare(),
+  integrations: [svelte(), icon(), mdx(), alpinejs(), db()],
+  vite: {
+    resolve: {
+      alias: {
+        'node-fetch': 'fetch',
+        debug: path.resolve(__dirname, './src/utils/debug-mock.js'),
+      },
+    },
+    plugins: [
+      tailwindcss(),
+      Fonts({
+        google: {
+          families: [{
+            name: 'Space Mono',
+            styles: 'ital,wght@0,400;0,700;1,400;1,700',
+          }],
+        },
+      }),
+    ]
+  }
 });
